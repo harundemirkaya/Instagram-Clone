99008eab922b263930dc81ca15a229d04ddb236e
Harun Demirkaya <harundmrk@gmail.com>
2022-08-31 12:32:01 +0300

    Complete Account Page

--- lib/pages/account_page.dart
+++ lib/pages/account_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
+import 'package:instaclone/util/account_images_json.dart';
 import 'package:instaclone/util/constant.dart';
 
 import '../theme/colors.dart';
@@ -13,6 +13,7 @@ class AccountPage extends StatefulWidget {
 }
 
 class _AccountPageState extends State<AccountPage> {
+  int selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
@@ -113,13 +114,210 @@ class _AccountPageState extends State<AccountPage> {
                         ),
                       ],
                     ),
-                  )
+                  ),
+                  Container(
+                    width: (size.width - 20) * 0.7,
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Column(
+                            children: [
+                              Text(
+                                "10",
+                                style: TextStyle(
+                                    fontSize: 18, fontWeight: FontWeight.bold),
+                              ),
+                              Text(
+                                "Posts",
+                                style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                    height: 1.5),
+                              ),
+                            ],
+                          ),
+                          Column(
+                            children: [
+                              Text(
+                                "331",
+                                style: TextStyle(
+                                    fontSize: 18, fontWeight: FontWeight.bold),
+                              ),
+                              Text(
+                                "Followers",
+                                style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                    height: 1.5),
+                              ),
+                            ],
+                          ),
+                          Column(
+                            children: [
+                              Text(
+                                "345",
+                                style: TextStyle(
+                                    fontSize: 18, fontWeight: FontWeight.bold),
+                              ),
+                              Text(
+                                "Following",
+                                style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                    height: 1.5),
+                              ),
+                            ],
+                          ),
+                        ]),
+                  ),
                 ],
-              )
+              ),
+              SizedBox(
+                height: size.height / 80,
+              ),
+              Text(
+                username,
+                style: TextStyle(color: bgDark, fontWeight: FontWeight.bold),
+              ),
+              Text(instagramBio),
+              SizedBox(
+                height: size.height / 80,
+              ),
+              Container(
+                height: size.height / 25,
+                width: (size.width - 20),
+                decoration: BoxDecoration(
+                  border: Border.all(width: 1, color: bgGrey),
+                  borderRadius: BorderRadius.circular(5),
+                  color: bgLightGrey,
+                ),
+                child: Center(
+                  child: Text(
+                    "Edit Profile",
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: size.height / 80,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Story Highlights",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Icon(Icons.keyboard_arrow_down),
+                ],
+              ),
             ],
           ),
-        )
+        ),
+        SizedBox(
+          height: size.height / 80,
+        ),
+        Container(
+          height: 0.5,
+          width: size.width,
+          decoration: BoxDecoration(
+            color: bgGrey.withOpacity(0.8),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(vertical: 3),
+          child: Row(children: [
+            Container(
+              width: (size.width * 0.5),
+              child: IconButton(
+                splashRadius: 20,
+                icon: Icon(
+                  Icons.grid_on_outlined,
+                  color: selectedIndex == 0 ? textBlack : textGrey,
+                ),
+                onPressed: () {
+                  setState(() {
+                    selectedIndex = 0;
+                  });
+                },
+              ),
+            ),
+            Container(
+              width: (size.width * 0.5),
+              child: IconButton(
+                splashRadius: 20,
+                icon: Icon(
+                  Icons.badge_outlined,
+                  color: selectedIndex == 1 ? textBlack : textGrey,
+                ),
+                onPressed: () {
+                  setState(() {
+                    selectedIndex = 1;
+                  });
+                },
+              ),
+            ),
+          ]),
+        ),
+        Column(
+          children: [
+            Row(
+              children: [
+                Container(
+                  height: 1,
+                  width: (size.width * 0.5),
+                  decoration: BoxDecoration(
+                      color: selectedIndex == 0 ? bgDark : Colors.transparent),
+                ),
+                Container(
+                  height: 1,
+                  width: (size.width * 0.5),
+                  decoration: BoxDecoration(
+                      color: selectedIndex == 1 ? bgDark : Colors.transparent),
+                ),
+              ],
+            ),
+            Container(
+              height: 0.5,
+              width: size.width,
+              decoration: BoxDecoration(
+                color: bgGrey.withOpacity(0.8),
+              ),
+            ),
+          ],
+        ),
+        SizedBox(
+          height: size.height / 90,
+        ),
+        IndexedStack(
+          index: selectedIndex,
+          children: [
+            getImages(size, selectedIndex),
+            getImages(size, selectedIndex),
+          ],
+        ),
       ],
     );
+  }
+
+  Widget getImages(size, indexSelect) {
+    return Wrap(
+        direction: Axis.horizontal,
+        spacing: 3,
+        runSpacing: 3,
+        children: List.generate(
+            indexSelect == 0 ? images.length : imageWithTags.length, (index) {
+          return Container(
+            height: 150,
+            width: (size.width - 6) / 3,
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: NetworkImage(indexSelect == 0
+                        ? images[index]
+                        : imageWithTags[index]),
+                    fit: BoxFit.cover)),
+          );
+        }));
   }
 }
